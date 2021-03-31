ffadb2c322254233d27c61422dcbeacce1d56680
yellowHuang <hlady1115@gmail.com>
2021-03-31 20:38:16 +0800

    add /auth/login

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const path = require('path')
+const fs = require('fs')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const path = require('path')
-const router = jsonServer.router(path.join(__dirname, 'user.json'))
+const router = jsonServer.router(path.join(__dirname, './data/user.json'))
 const middlewares = jsonServer.defaults()
 
 // Set default middlewares (logger, static, cors and no-cache)
@@ -10,6 +11,32 @@ server.use(middlewares)
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
+
+
+const getUserDb = () => {
+    return JSON.parse(fs.readFileSync(path.join(__dirname, './data/user.json'), 'UTF-8'))
+}
+
+const isAuthenticated = ({ email, password }) => {
+    return (
+        getUserDb().users.findIndex(
+            user => user.email === email && user.password === password
+        ) !== -1
+    )
+}
+
+// auth
+server.post('/auth/login', (req, res) => {
+    const { email, password } = req.body;
+    if(isAuthenticated({ email, password })) {
+        const token = 'saddsfdsferfdvf.sfrefdcdscds.09878987678sdf'
+        return res.status(200).json(token);
+    } else {
+        console.log('login fail');
+        return res.status(401).json('login fail');
+    }
+})
+
 
 
 // config
