2007df4e7e8ce80613e25e508b4d26ff32f1d093
yellowHuang <hlady1115@gmail.com>
2021-03-31 22:05:56 +0800

    add create token

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const path = require('path')
 const fs = require('fs')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, './data/user.json'))
 const middlewares = jsonServer.defaults()
+const jwt = require('jsonwebtoken')
+
+const router = jsonServer.router(path.join(__dirname, './data/user.json'))
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
@@ -13,27 +15,34 @@ server.use(middlewares)
 server.use(jsonServer.bodyParser)
 
 
-const getUserDb = () => {
-    return JSON.parse(fs.readFileSync(path.join(__dirname, './data/user.json'), 'UTF-8'))
-}
+// 取得使用者資料
+const getUserDb = () => JSON.parse(fs.readFileSync(path.join(__dirname, './data/user.json'), 'UTF-8'))
 
+// 驗證使用者
 const isAuthenticated = ({ email, password }) => {
     return (
-        getUserDb().users.findIndex(
-            user => user.email === email && user.password === password
-        ) !== -1
+        getUserDb().users.findIndex(user => user.email === email && user.password === password) !== -1
     )
 }
+
+const SECRET = '1234567890yyyylalala';
+const expiresIn = '1h';
+const cerateToken = payload => jwt.sign(payload, SECRET, { expiresIn })
+
 
 // auth
 server.post('/auth/login', (req, res) => {
     const { email, password } = req.body;
+
     if(isAuthenticated({ email, password })) {
-        const token = 'saddsfdsferfdvf.sfrefdcdscds.09878987678sdf'
+        const user = getUserDb().users.find(u => u.email === email && u.password === password);
+        const { nickname, type } = user;
+        const token = cerateToken({ nickname, type });
         return res.status(200).json(token);
     } else {
-        console.log('login fail');
-        return res.status(401).json('login fail');
+        const status = 401;
+        const message = 'login fail';
+        return res.status(401).json({ status, message });
     }
 })
 
